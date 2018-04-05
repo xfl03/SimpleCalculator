d44c1a9787ecbc6a0907463833671768e1c6550d
Alexander Xia <xfl03@hotmail.com>
2018-04-05 10:37:42 +0800

    Fix bug for negative

--- src/main/kotlin/simplecalculator/Calculator.kt
+++ src/main/kotlin/simplecalculator/Calculator.kt
@@ -28,7 +28,7 @@ class Calculator(private val expression: String) {
         var negativeFlag = false//Is next number negative?
         var lastOp = Operator.PLUS// last Useful Operator
         var index = -1
-        var lastPoint=-1
+        var lastPoint = -1
 
         nums.push(Fraction(0))
         ops.push(Operator.PLUS)
@@ -43,31 +43,30 @@ class Calculator(private val expression: String) {
             if (!util.isLegal(it))
                 throw ParseException("illegal char '${it.toChar()}'", index)
 
-            if(it.toChar()=='.'){
-                if(lastPoint>=0)
-                    throw ParseException("too more '.'",index)
-                lastPoint=0
-                if(!numFlag)
+            if (it.toChar() == '.') {
+                if (lastPoint >= 0)
+                    throw ParseException("too more '.'", index)
+                lastPoint = 0
+                if (!numFlag)
                     nums.push(Fraction(0))
-                numFlag=true
-            }else if (util.isNum(it)) {
+                numFlag = true
+            } else if (util.isNum(it)) {
                 var num = util.toNum(it).toLong()
-                if (negativeFlag) {
-                    negativeFlag = false
+                if (negativeFlag)
                     num = -num
-                }
+
                 if (numFlag)
-                    if(lastPoint>=0)
-                        nums.push(nums.pop() .add(Fraction(num.toLong(),util.pow(10,lastPoint+1))))
-                        else
-                    nums.push(nums.pop() .multiply(Fraction(10)).add(Fraction(num)))
+                    if (lastPoint >= 0)
+                        nums.push(nums.pop().add(Fraction(num, util.pow(10, lastPoint + 1))))
+                    else
+                        nums.push(nums.pop().multiply(Fraction(10)).add(Fraction(num)))
                 else
                     nums.push(Fraction(num))
 
                 firstFlag = false
                 numFlag = true
             } else {
-                lastPoint=-1
+                lastPoint = -1
                 val op = util.getOperator(it)!!
 
                 if (firstFlag || (!numFlag && !util.isRightBracket(lastOp))) {
@@ -85,7 +84,9 @@ class Calculator(private val expression: String) {
                     }
                 }
 
-                val preOp = ops.pop()
+                negativeFlag = false
+
+                var preOp = ops.pop()
                 if (util.isRightBracket(op)) {
                     if (!numFlag && !util.canEndWith(lastOp))
                         throw ParseException("no right number for '${lastOp.ch}'", index - 1)
@@ -94,7 +95,7 @@ class Calculator(private val expression: String) {
                         try {
                             compute { it.priority != op.priority }
                         } catch (e: EmptyStackException) {
-                            throw ParseException("no left bracket for '${op.ch}'",index)
+                            throw ParseException("no left bracket for '${op.ch}'", index)
                         }
                         ops.pop()
                     }
@@ -106,11 +107,17 @@ class Calculator(private val expression: String) {
                 numFlag = false
                 lastOp = op
 
-                if (preOp.priority < op.priority || util.isLeftBracket(preOp))
+                if (preOp.priority < op.priority || util.isLeftBracket(preOp) || ops.isEmpty())
                     ops.push(preOp)
                 else {
-                    ops.push(preOp)
-                    compute { it.priority > op.priority && !util.isLeftBracket(it) }
+                    compute(preOp)
+
+                    //For decline
+                    preOp = ops.pop()
+                    if (ops.isNotEmpty() && preOp.priority > op.priority && !util.isLeftBracket(preOp))
+                        compute(preOp)
+                    else
+                        ops.push(preOp)
                 }
                 ops.push(op)
             }
@@ -125,40 +132,25 @@ class Calculator(private val expression: String) {
 
     @Throws(ParseException::class)
     private fun compute(condition: (Operator) -> (Boolean)) {
-        val numsTemp = Stack<Fraction>()
-        val opsTemp = Stack<Operator>()
-        numsTemp.push(nums.pop())
-
-        //Convert for * / ^
-        var temp: Operator = ops.pop()
-        while (condition(temp) && !util.canBeginWith(temp)) {
-            opsTemp.push(temp)
-            numsTemp.push(nums.pop())
-            temp = ops.pop()
-        }
-        ops.push(temp)
-
-        while (opsTemp.isNotEmpty()) {
-            temp = opsTemp.pop()
-            numsTemp.push(util.computeOperator(numsTemp.pop(), temp, numsTemp.pop()))
-        }
-        nums.push(numsTemp.pop())
-
         //Back forward compute
-        temp = ops.pop()
+        var temp = ops.pop()
         while (condition(temp)) {
-            if (util.isLeftBracket(temp)){
-                if(!fixRight)
+            if (util.isLeftBracket(temp)) {
+                if (!fixRight)
                     throw ParseException("no right bracket for '${temp.ch}'", expression.indexOf(temp.ch))
-                temp=ops.pop()
+                temp = ops.pop()
                 continue
             }
-            val right = nums.pop()
-            val left = nums.pop()
-            nums.push(util.computeOperator(left, temp, right))
+            compute(temp)
             temp = ops.pop()
         }
         ops.push(temp)
+    }
+
+    private fun compute(operator: Operator) {
+        val right = nums.pop()
+        val left = nums.pop()
+        nums.push(util.computeOperator(left, operator, right))
     }
 
     override fun toString() = "$expression=$result"
